4532f98743a8fe04a17d0f06682038774a5d93f0
Pavel Apanovskiy <pavelapanovskiy@MacBook-Pro-Pavel.local>
2018-10-23 20:56:46 +0300

    Initial commit

---
+++ app/src/main/java/com/mw2c/pipboy200011/motionlayoutlecture/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mw2c.pipboy200011.motionlayoutlecture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
